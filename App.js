28cee52fca818811c81f66ec37c1bd096f601880
Eduardo santana da cruz <eduardo.santana95@hotmail.com>
2022-12-01 14:40:57 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  Platform,
+  StyleSheet,
+  StatusBar as status,
+  SafeAreaView,
+  View,
+  ActivityIndicator,
+} from "react-native";
+import { WebView } from "react-native-webview";
 
 export default function App() {
+  const [urlhiperfar] = useState("http://d4500d653bcc.sn.mynetname.net:8079/m");
+  const [urlorto] = useState("http://orto.ddns.com.br:8079/m");
+  const [load, setLoad] = useState(true);
+  function carregar() {
+    setLoad(false);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <WebView
+        incognito={true}
+        style={styles.container1}
+        source={{ uri: "http://orto.ddns.com.br:8079/m" }}
+        onLoad={carregar}
+      />
+
+      {load && (
+        <ActivityIndicator
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+            height: "100%",
+          }}
+          size={40}
+          color={"#0000ff"}
+          
+        />
+          
+        
+      )}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  container1: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: Platform.OS === "android" ? status.currentHeight : 0,
   },
 });
